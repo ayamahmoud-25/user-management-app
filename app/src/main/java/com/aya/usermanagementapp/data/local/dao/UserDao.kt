2281ae198eb92fdfe47b507359c3f43eddd02b79
aya <ayamahmoudmsd005@gmail.com>
2026-07-03 13:16:46 +0300

    Add UserDao with insert and query methods

--- app/src/main/java/com/aya/usermanagementapp/data/local/dao/UserDao.kt
+++ app/src/main/java/com/aya/usermanagementapp/data/local/dao/UserDao.kt
@@ -1,4 +1,31 @@
 package com.aya.usermanagementapp.data.local.dao
 
-class UserDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.aya.usermanagementapp.data.local.entity.UserEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface UserDao {
+
+    @Insert
+    suspend fun insertUser(user: UserEntity)
+
+
+    @Query("SELECT * FROM users")
+    fun getAllUsers(): Flow<List<UserEntity>>
+
+
+    @Delete
+    suspend fun deleteUser(user: UserEntity)
+
+
+    @Update
+    suspend fun updateUser(user: UserEntity)
+
 }
+
+
--- app/src/main/java/com/aya/usermanagementapp/data/local/entity/UserEntity.kt
+++ app/src/main/java/com/aya/usermanagementapp/data/local/entity/UserEntity.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "users")
 data class UserEntity(
 
-    PrimaryKey(autoGenerate = true)
+    @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
 
     val name: String,
